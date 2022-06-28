421b7cd352b648e01fcd72c832a55ed1a1b5a258
Vinicius Coelho <vinicoelhose@gmail.com>
2022-06-27 22:37:35 -0300

    Remove charSet param from random generator

--- src/types/gen/pseudo_rand_string.go
+++ src/types/gen/pseudo_rand_string.go
@@ -20,19 +20,25 @@ func NewPseudoRandomString() PseudoRand {
 	}
 }
 
-func (r PseudoRand) Generate(length int) string {
-	s := r.newString(length, charSet)
-	// ensures that a random string is generated
-	for _, exists := r.values[s]; exists; s = r.newString(length, charSet) {
+// GenerateUnique returns a non previously generated pseudo random string
+func (r PseudoRand) GenerateUnique(length int) string {
+	s := r.newString(length)
+	// Ensures that a random string is generated
+	for _, exists := r.values[s]; exists; s = r.newString(length) {
 	}
 	r.values[s] = struct{}{}
 	return s
 }
 
-func (r PseudoRand) newString(length int, charset string) string {
+// GenerateAny returns a pseudo random string
+func (r PseudoRand) GenerateAny(length int) string {
+	return r.newString(length)
+}
+
+func (r PseudoRand) newString(length int) string {
 	b := make([]byte, length)
 	for i := range b {
-		b[i] = charset[r.seed.Intn(len(charset))]
+		b[i] = charSet[r.seed.Intn(len(charSet))]
 	}
 	return string(b)
 }
