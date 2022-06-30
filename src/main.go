048b98c52e6c73a2f30d1aa410fee12b14bb670d
Vinicius Coelho <vinicoelhose@gmail.com>
2022-06-29 22:43:09 -0300

    Update main to include routes initialization

--- src/main.go
+++ src/main.go
@@ -2,12 +2,22 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
+	"net/http"
+	"time"
 
+	"posterr/src/router"
+	"posterr/src/storage"
 	storagedb "posterr/src/storage/db"
+
+	"github.com/rs/cors"
 )
 
-var initDB = flag.Bool("init-db", false, "creates a database and its tables")
+var (
+	initDB = flag.Bool("init-db", false, "creates a database and its tables")
+	port   = flag.Int("port", 3000, "application port ")
+)
 
 func main() {
 	flag.Parse()
@@ -19,32 +29,28 @@ func main() {
 		}
 	}
 
-	/*
-		ls, err := storage.NewLottoBacked()
-		if err != nil {
-			log.Fatalf("could not initialize storage: %s", err)
-		}
+	posts := storage.NewPosterrBacked(db)
+	users := storage.NewUserBacked(db, posts)
 
-		r := router.CreateRoutes(ls)
-		c := cors.New(cors.Options{
-			AllowedOrigins: []string{"*"},
-			AllowedMethods: []string{
-				http.MethodGet,
-				http.MethodPost,
-				http.MethodDelete,
-			},
-			AllowedHeaders:   []string{"Accept", "Content-Type"},
-			AllowCredentials: false,
-		})
-		handler := c.Handler(r)
+	r := router.CreateRoutes(posts, users)
+	c := cors.New(cors.Options{
+		AllowedOrigins: []string{"*"},
+		AllowedMethods: []string{
+			http.MethodGet,
+			http.MethodPost,
+			http.MethodDelete,
+		},
+		AllowedHeaders:   []string{"Accept", "Content-Type"},
+		AllowCredentials: false,
+	})
+	handler := c.Handler(r)
 
-		s := &http.Server{
-			Handler:      handler,
-			ReadTimeout:  0,
-			WriteTimeout: 0,
-			Addr:         ":3000",
-			IdleTimeout:  time.Second * 60,
-		}
-		log.Fatal(s.ListenAndServe())
-	*/
+	s := &http.Server{
+		Handler:      handler,
+		ReadTimeout:  0,
+		WriteTimeout: 0,
+		Addr:         fmt.Sprintf(":%d", *port),
+		IdleTimeout:  time.Second * 60,
+	}
+	log.Fatal(s.ListenAndServe())
 }
